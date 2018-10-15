7aba72c53989b75ede7c1f76d109ca933cb9cba9
Tyler Roberts <troberts@Tyler-Roberts.local>
2018-10-15 12:38:50 -0400

    made some pep8 corrections

--- bmo.py
+++ bmo.py
@@ -1,28 +1,31 @@
-import pygame
 import random
 import glob
+import pygame
 
-WIDTH  = 800
+WIDTH = 800
 HEIGHT = 600
+
 
 class MainWindow:
     def __init__(self):
         pygame.init()
-        self.display = pygame.display.set_mode((WIDTH, HEIGHT), pygame.RESIZABLE)
+        self.display = pygame.display.set_mode(
+            (WIDTH, HEIGHT), pygame.RESIZABLE)
         self.faces = glob.glob('assets/faces/*.jpg')
         pygame.display.set_caption('BMO ♥')
-		
-    def _setRandomFace(self):
+
+    def _set_random_face(self):
         face = self.faces[random.randint(0, len(self.faces) - 1)]
-        self.display.blit(pygame.image.load(face), (0,0))
-		
+        self.display.blit(pygame.image.load(face), (0, 0))
+
     def run(self):
-        self._setRandomFace()
+        self._set_random_face()
         while True:
             pygame.display.update()
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     return
+
 
 if __name__ == '__main__':
     mw = MainWindow()
